8ad03570c5980fba154fe494af4f4a16ca27a026
Nanask <ybhag1005@daum.net>
2021-08-17 14:56:36 +0900

    fregment

--- movies/app/src/main/java/com/callor/movies/MainActivity.java
+++ movies/app/src/main/java/com/callor/movies/MainActivity.java
@@ -6,9 +6,11 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -17,6 +19,7 @@ import com.callor.movies.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -66,6 +69,59 @@ ActionBar에 메뉴를 그리는 코드가 작성된다.
 //        R.menu.menu_main불러와서 menu에 넣어라
 //       여길 수정하려면 menu를 수정하면 된다.
 
+        /*
+        ActionBar에 구현된 검색창을 활성화 하기 위한 코드
+        android.위젯.searchView를 사용하여 객체 생성
+         */
+        SearchView searchView = (SearchView) menu.findItem(R.id.app_bar_search).getActionView();
+
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        searchView.setQueryHint("영화 검색");
+
+//        키보드에 검색 버튼을 클릭했을때
+//        검색창이 활성화 되었을 때 실행되는 event
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+//            키보드의 검색 버튼 클릭
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Log.d("검색어",s);
+                /*
+                검색창에 입력한 검색어를 secondFragment에게 보내고
+                SecondFragment에서는 전달받은 검색어를 사용하여
+                Naver Open API 영화검색을 수행하여
+                RecyclerView에 표시하기기
+                */
+
+                //검색창에 입력된 값이 있으면
+                if( !s.isEmpty() ) {
+                    /*
+                    safe-args plugin을
+                     프로젝트의 build.gradle에 설정하면
+                    nav_Graph.xml에 선언된
+                    fragment ID를 참조하기 위한
+                    클래스가 자동으로 생성된다.
+
+                    여기서 생성된 action 객체에 fragment에 전달할 데이터를 실어서 보낸다.
+                     */
+                    NavDirections action =
+                    FirstFragmentDirections.actionFirstFragmentToSecondFragment();
+
+                    // 생성된 action에 따라 화면에
+                    // sectionFragment를 띄운다.
+                    NavController controller = Navigation.findNavController(MainActivity.this, R.id.nav_host_fragment_content_main);
+                    controller.navigate(action);
+                }
+
+                return false;
+            }
+//          검색창에 문자열을 입력할 때
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
         return true;
     }
 
@@ -76,8 +132,19 @@ ActionBar에 메뉴를 그리는 코드가 작성된다.
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
 
-            Toast.makeText(MainActivity.this, "설정메뉴클릭"
-            ,Toast.LENGTH_LONG).show();
+/*
+MainActivity 에 firstFragment가 열린 상태에서 ActionBar의 settings 메뉴를 클릭하면
+firstFragment가 있던 공간에 SettingsFragment를 보여주기
+
+1. content_main.xml의 fragment view를 NavController로 설정하고
+2. NavController에 navigate를 실행하는데
+이때 navGraph.xml에 설정된 Action을 사용하여 실행을 한다.
+ */
+    NavController controller =
+            Navigation.findNavController(MainActivity.this,
+                    R.id.nav_host_fragment_content_main);
+    controller.navigate(R.id.action_FirstFragment_to_SettingsFragment);
+
             return true;
         }
 
