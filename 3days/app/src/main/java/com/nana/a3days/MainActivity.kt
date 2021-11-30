6f29a665e7ee5c2072fe6282eabfb283430f0b96
Nanask <ybhag1005@daum.net>
2021-11-30 23:47:22 +0900

    instagram clone

---
+++ 3days/app/src/main/java/com/nana/a3days/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.nana.a3days
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nana.a3days.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ 3days/app/src/main/java/com/nana/a3days/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.nana.a3days.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
